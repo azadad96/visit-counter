4db906fa4d4c63815ae8a38c7640dc34b413277c
azadad96 <azadad6@pm.me>
2020-06-06 19:21:06 +0200

    can export data as csv now

--- firefox/data.js
+++ firefox/data.js
@@ -1,9 +1,39 @@
-function configure() {
-	window.open("configure.html");
-}
-
 window.onload = () => {
-	document.getElementById("config").onclick = configure;
+	document.getElementById("config").onclick = () => {
+		window.open("configure.html");
+	};
+
+	document.getElementById("export").onclick = () => {
+		var getVisited = browser.storage.local.get();
+		getVisited.then((res) => {
+			var data = "website,visits";
+			var elements = [];
+			for (var i of Object.keys(res)) {
+				if (i == "config")
+					continue;
+				elements.push({
+					url: i,
+					visits: res[i]
+				});
+			}
+			elements.sort((a, b) => (a.visits > b.visits) ? 0 : 1);
+
+			for (var i of elements)
+				data += "\n" + i.url + "," + i.visits;
+
+			var blob = new Blob([data], {type: 'text/csv'});
+		    if (window.navigator.msSaveOrOpenBlob) {
+		        window.navigator.msSaveBlob(blob, "visit_counter.csv");
+		    } else {
+		        var a = window.document.createElement('a');
+		        a.href = window.URL.createObjectURL(blob);
+		        a.download = "visit_counter.csv";        
+		        document.body.appendChild(a);
+		        a.click();        
+		        document.body.removeChild(a);
+		    }
+		});
+	};
 
 	var dataDiv = document.getElementById("data");
 
@@ -49,6 +79,8 @@ window.onload = () => {
 		}
 		document.getElementById("config").style.backgroundColor = res.config.foreground;
 		document.getElementById("config").style.color = res.config.text;
+		document.getElementById("export").style.backgroundColor = res.config.foreground;
+		document.getElementById("export").style.color = res.config.text;
 		document.body.style.fontSize = res.config.font;
 		document.body.style.backgroundColor = res.config.background;
 	});
